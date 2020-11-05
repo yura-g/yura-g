d1f1e85e47ba4e44ee2caf513bedaeb7d1a01b02
yura-g <y.gaidush@gmail.com>
2020-11-06 00:32:52 +0200

    Version v1.0.0"

--- run_webserver.sh
+++ run_webserver.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-while true; do echo -e "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\nVersion v1.0.2" | nc -l -p 8080; done
+while true; do echo -e "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\nVersion v1.0.0" | nc -l -p 8080; done
